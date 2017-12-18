d09d77ec0a1a4001d9fa0a443571cec103432657
dedeusdiego <34646174+dedeusdiego@users.noreply.github.com>
2017-12-18 12:48:17 -0200

    Add files via upload

---
+++ Escravo2.ino
@@ -0,0 +1,41 @@
+//escravo 2
+int endereco = 2;
+byte recByte;
+const int LDR = A0;
+uint8_t PinD1 = D4; 
+int input_val = 0;
+
+//pin configuration
+void setup(){
+  Serial.begin(9600);
+  pinMode(PinD1, OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(PinD1,HIGH);
+}
+
+void loop(){
+ input_val  = analogRead(LDR);
+ if(Serial.available()){
+   recByte = Serial.read();
+ }
+
+ if(recByte == 1){ 
+   if(digitalRead(LED_BUILTIN)==LOW){
+  digitalWrite(LED_BUILTIN,HIGH);
+  digitalWrite(PinD1,LOW);
+   Serial.println("liguei o led");
+   }
+   else{
+    digitalWrite(LED_BUILTIN,LOW);
+    digitalWrite(PinD1,HIGH);
+    Serial.println("desliguei o led");
+   }
+    recByte = 0;
+ }
+
+ if(recByte == 2){ 
+  Serial.write(input_val);
+ }
+}
+
+
---
+++ Server.ino
@@ -0,0 +1,204 @@
+//------------------------------------------------------------------------------------
+// Libraries Needed For This Project
+//------------------------------------------------------------------------------------
+  #include <Wire.h>           // To Be Able To Use Sensors Like IMU9050
+  #include <EEPROM.h>         // To Be Able To Store & Read Data Between Power Off
+  #include <ESP8266WiFi.h>    // The Basic Function Of The ESP NOD MCU
+//------------------------------------------------------------------------------------
+// Defining I/O Pins
+//------------------------------------------------------------------------------------
+  #define       LED0      2         // WIFI Module LED
+//------------------------------------------------------------------------------------
+// LED Delay Variables
+//------------------------------------------------------------------------------------
+  int           LEDState          = LOW;
+  unsigned long CurrMillis        = 0;
+  unsigned long PrevMillis        = 0;
+  unsigned long Interval          = 1000;
+//------------------------------------------------------------------------------------
+// WIFI Authentication Variables
+//------------------------------------------------------------------------------------
+/* This Client Is Going To Connect To A WIFI Server Access Point
+ * So You Have To Specify Server WIFI SSID & Password In The Code Not Here
+ * Please See The Function Below Name (WiFi.Begin)
+ * If WIFI dont need Any password Then WiFi.begin("SSIDNAME")
+ * If WIFI needs a Password Then WiFi.begin("SSIDNAME", "PASSWORD")
+ */
+  char*         TKDssid;            // Wifi Name
+  char*         TKDpassword;        // Wifi Password
+//------------------------------------------------------------------------------------
+// WIFI Module Role & Port
+//------------------------------------------------------------------------------------
+/* This WIFI Module Code Works As A Client
+ * That Will Connect To A Server WIFI Modul With (IP ADDress 192.168.4.1)
+ */
+  int             TKDServerPort  = 9001;
+  IPAddress       TKDServer(192,168,4,1);
+  WiFiClient      TKDClient;
+//====================================================================================
+
+  void setup() 
+  {
+    // Setting The Serial Port ----------------------------------------------
+    Serial.begin(9600);           // Computer Communication
+    
+    // Setting The Mode Of Pins ---------------------------------------------
+    pinMode(LED0, OUTPUT);          // WIFI OnBoard LED Light
+    digitalWrite(LED0, !LOW);       // Turn WiFi LED Off
+    
+    // Print Message Of I/O Setting Progress --------------------------------
+    Serial.println("\nI/O Pins Modes Set .... Done");
+
+    // Starting To Connect --------------------------------------------------
+    if(WiFi.status() == WL_CONNECTED)
+    {
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF);
+      delay(50);
+    }
+
+    /* in this part it should load the ssid and password 
+     * from eeprom they try to connect using them */
+    
+    WiFi.mode(WIFI_STA);            // To Avoid Broadcasting An SSID
+    WiFi.begin("TAKEONE", "");      // The SSID That We Want To Connect To
+
+    // Printing Message For User That Connetion Is On Process ---------------
+    Serial.println("!--- Connecting To " + WiFi.SSID() + " ---!");
+
+    // WiFi Connectivity ----------------------------------------------------
+    CheckWiFiConnectivity();        // Checking For Connection
+
+    // Stop Blinking To Indicate Connected ----------------------------------
+    digitalWrite(LED0, !HIGH);
+    Serial.println("!-- Client Device Connected --!");
+
+    // Printing IP Address --------------------------------------------------
+    Serial.println("Connected To      : " + String(WiFi.SSID()));
+    Serial.println("Signal Strenght   : " + String(WiFi.RSSI()) + " dBm");
+    Serial.print  ("Server IP Address : ");
+    Serial.println(TKDServer);
+    Serial.print  ("Server Port Num   : ");
+    Serial.println(TKDServerPort);
+    // Printing MAC Address
+    Serial.print  ("Device MC Address : ");
+    Serial.println(String(WiFi.macAddress()));
+    // Printing IP Address
+    Serial.print  ("Device IP Address : ");
+    Serial.println(WiFi.localIP());
+    
+    // Conecting The Device As A Client -------------------------------------
+    TKDRequest();
+  }
+
+//====================================================================================
+  
+  void loop()
+  {
+    int opcao = 0;
+    while(opcao==0){
+       if(Serial.available() > 0){
+       opcao = Serial.parseInt();  
+       }
+    }
+    
+    if (opcao == 1){//atuar escravo 1
+      TKDClient.println("1");
+      delay(2000);
+    }
+    
+    if (opcao == 2){//atuar escravo 2
+      Serial.println("Entrou para atuar escravo2");
+      TKDClient.println("2");
+      delay(2000);
+    }
+    
+    if (opcao == 3){//ler sensor escravo 2
+        TKDClient.println("3");
+        delay(3000);
+        while(TKDClient.available())
+        {
+          String Message = TKDClient.readStringUntil('\r');
+          TKDClient.flush();
+          Serial.println(Message);
+        }
+        TKDClient.flush();
+    }
+  }
+
+//====================================================================================
+
+  void ReadButton()
+  {
+          Serial.println("<-SCORED>");
+          TKDClient.println("<-SCORED>");
+          delay(2000);
+          while(TKDClient.available())
+          {
+            String Message = TKDClient.readStringUntil('\r');
+            TKDClient.flush();
+            Serial.println("Resposta: " + Message);
+          }
+          TKDClient.flush();
+        //}
+      //}
+    //}
+
+    // Last Button State Concidered
+    //LastButtonState = reading;
+  }
+
+//====================================================================================
+
+  void CheckWiFiConnectivity()
+  {
+    while(WiFi.status() != WL_CONNECTED)
+    {
+      for(int i=0; i < 10; i++)
+      {
+        digitalWrite(LED0, !HIGH);
+        delay(250);
+        digitalWrite(LED0, !LOW);
+        delay(250);
+        Serial.print(".");
+      }
+      Serial.println("");
+    }
+  }
+
+//====================================================================================
+
+  void I2C_Begin()
+  {
+    /* ----------------------------------------------------------------------
+     * Setting The I2C Pins SDA, SCL
+     * Because We Didnt Specify Any Pins The Defult
+     * SDA = D4 <GPIO2>, SCL = D5 <GPIO14> For ESP8266 Dev Kit Node MCU v3
+     --------------------------------------------------------------------- */
+    // Frequency Of The I2C
+    #define TWI_FREQ  400000L       // I2C Frequency Setting To 400KHZ
+    
+    // If Ever You Needed To Use Wire I2C
+    Wire.begin();                   // Begginning The I2C
+    
+    // Setting Up The I2C Of The MPU9250 ------------------------------------
+    Wire.setClock(TWI_FREQ);        // Setting The Frequency MPU9250 Require
+  }
+        
+//====================================================================================
+
+  void TKDRequest()
+  {
+    // First Make Sure You Got Disconnected
+    TKDClient.stop();
+
+    // If Sucessfully Connected Send Connection Message
+    if(TKDClient.connect(TKDServer, TKDServerPort))
+    {
+      Serial.println    ("<-CONNECTED>");
+      TKDClient.println ("<-CONNECTED>");
+    }
+  }
+
+//====================================================================================
+
